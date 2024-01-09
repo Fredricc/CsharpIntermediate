de5dcd4acbf6359d0446d324ad23a6d19fbf65b3
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-10 00:47:59 +0300

    methods

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace CsharpIntermediate
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
+
+             var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            point.Move(new Point(100, 200));
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+
+
             var customer = new Customer(1, "Fresh");
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
