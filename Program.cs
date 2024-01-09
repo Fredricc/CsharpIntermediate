119d1ca97185f4865aa0d4666f1ea885f069d686
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-01-09 05:20:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CsharpIntermediate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "Fresh");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
